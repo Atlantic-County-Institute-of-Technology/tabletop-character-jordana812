6ebcb85bc848c5c528d836f224f1e3caec8106f2
jordan3 <arri8330@student.acitech.org>
2025-03-31 14:12:21 -0400

    cute cubular cards

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+
+}
+
+class character{
+    protected String name;
+    protected int level;
+    protected int strength;
+    protected int dexterity;
+
+}
