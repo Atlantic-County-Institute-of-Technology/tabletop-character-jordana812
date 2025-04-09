ba0994642988dda782cf9a24a873cc87a1058fe7
jordan3 <arri8330@student.acitech.org>
2025-04-09 13:34:42 -0400

    FINALLY DONE OH MY GOD
    completed

--- tabletop/src/Main.java
+++ tabletop/src/Main.java
@@ -1,17 +1,20 @@
-
+//Jordan Arrington
+//4-9-25
+//TableTop DND Characters
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
+//Creates a new wizard character
 public class Main {
     public static void main(String[] args){
-        Character c1 = new Character();
-
-        System.out.println();
+        Character c1 = new Wizard("ROBERT BAEZZZ", 2);
+        System.out.println(c1);
     }
 }
 
-class characterStats{
+//Sets all character class stats
+class Character{
     protected String name;
     protected int level;
     protected int HP;
@@ -24,8 +27,10 @@ class characterStats{
     protected int charizz;
     protected ArrayList<String> features;
 
-    public void Character(){
-        this.name = "Jordan Arrington";
+    //Character within the character class, which sets this as the default
+    public Character(){
+        features = new ArrayList<>();
+        this.name = "Jordan";
         this.level = 1;
         this.STR = 10;
         this.DEX = 10;
@@ -35,18 +40,26 @@ class characterStats{
         this.wizzdom = 10;
         this.charizz = 10;
         hpCALC();
+        armorClassCALC();
         }
 
-    public void Character(String name, int level){
+    //Sets the character stats to randomize at a bound between 3 and 18, calls the name of the character and level as well. Takes HP calc function and armor calc function into account.
+    public Character(String name, int level){
         Random rand = new Random();
+        features = new ArrayList<>();
         STR = rand.nextInt(3, 18);
         DEX = rand.nextInt(3, 18);
         INT = rand.nextInt(3, 18);
         CON = rand.nextInt(3, 18);
         wizzdom = rand.nextInt(3, 18);
         charizz = rand.nextInt(3, 18);
+        this.name = name;
+        this.level = level;
+        hpCALC();
+        armorClassCALC();
     }
 
+    //Calculates the HP of the character, adding 10 + its constitution if it is level 1, else normal HP calculations
     public void hpCALC(){
         if(level == 1){
             HP = 10 + CON;
@@ -56,14 +69,17 @@ class characterStats{
         }
     }
 
+    //armor class determined with 10 + its dexterity
     public void armorClassCALC(){
         armor_class = 10 + DEX;
     }
 
-    public void abilityMOD(){
-
+    //ability modifier takes its ability score - 10 divided by 2
+    public int abilityMOD(int score){
+        return (score - 10) / 2;
     }
 
+    //When character levels up, its level adds by 1 and HP + armor class is recalculated
     public void levelUP(){
         level++;
         hpCALC();
@@ -71,20 +87,34 @@ class characterStats{
     }
 
 
+    //function to add features to character
     public void ADDfeatures(String feature) {
         features.add(feature);
     }
 
+    //What is printed to display character and its stats
     public String toString(){
         return "\nCharacter: " + this.name + "\nLVL: " + this.level + "\nSTR: " + this.STR + "\nDEX: " + this.DEX + "\nINT: " + this.INT + "\nConstipation: " + this.CON + "\nArmor: " + this.armor_class + "\nWizzdom: " + this.wizzdom + "\nCharizz: " + this.charizz;
     }
 }
 
+//Wizard class, which is a character type. Has special attributes as stated in function
 class Wizard extends Character{
-    public void wizard(){
+
+    public Wizard(){
         super();
+        INT = INT + 2;
+        CON = CON + 1;
+        ADDfeatures("Arcane Recovery");
+        ADDfeatures("Spell Mastery");
+    }
+    public Wizard(String name, int level) {
+        super(name, level);
+        INT = INT + 2;
+        CON = CON + 1;
+        ADDfeatures("Arcane Recovery");
+        ADDfeatures("Spell Mastery");
 
     }
-
 }
 
